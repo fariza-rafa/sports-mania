e07f65f0cc0a867bc8af0a898d6c8976b56bc0a7
Fariza Rafa <farizarafa123@gmail.com>
2021-03-11 22:17:41 +0600

    all done

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="copyright">
+            <p>Copyright Sports Mania. All Rights Reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
